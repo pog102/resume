c46e1deeadbe2b3cf8bc811f36043fcfd0674d7f
Your Name <you@example.com>
2023-08-12 16:31:26 +0000

    add

--- js/steam.js
+++ js/steam.js
@@ -1,26 +1,26 @@
-const xmlUrl = 'https://steamcommunity.com/id/pogpog69420?xml=1';
-
-// Fetch the XML data
-fetch(xmlUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Network response was not ok (${response.status} ${response.statusText})`);
-    }
-    return response.text();
-  })
-  .then(xmlData => {
-    // Here, you can parse and work with the XML data as needed
-    console.log(xmlData);
-    
-    // For example, you can parse the XML using DOMParser
-    // const parser = new DOMParser();
-    // const xmlDoc = parser.parseFromString(xmlData, 'application/xml');
-    // 
-    // // Now you can work with the parsed XML document (xmlDoc)
-    // console.log(xmlDoc);
-  })
-  .catch(error => {
-    console.error('Error fetching XML:', error);
-  });
-
-
+  // Read and process logo links from logos.txt
+  fetch('runners/links.html')
+    .then(response => response.text())
+    .then(data => {
+      const logoContainer = document.getElementById('logoContainer');
+            // logoContainer.style.marginLeft = '98px'; // Adjust the left margin here
+      const logoLinks = data.split('\n');
+            // logoContainer.style.max-width = '79%';
+            // logoContainer.style.textAlign = 'center';
+      for (const link of logoLinks) {
+        if (link.trim() !== '') {
+          const img = document.createElement('img');
+          img.src = `${link}`;
+          img.alt = 'Game Logo1';
+         img.style.width = '300px'; // Adjust the image size her
+        img.style.margin = '10px'; // Adjust the margin space here
+         img.style.position = 'relative';
+            img.style.zIndex = '1';
+                      // img.style.marginLeft = '-100px'; // Adjust the overlap distance here
+          logoContainer.appendChild(img);
+        }
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching and displaying logo links:', error);
+    });
